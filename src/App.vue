efeec7a460767b5b6401448893c88e9dfdc3185b
li1553770945 <1553770945@qq.com>
2021-10-28 10:19:36 +0800

    新建项目

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-row class="tac">
+    <el-col :span="5">
+      <el-menu default-active="1" class="el-menu-vertical-demo">
+        <el-menu-item index="1">
+          <i class="el-icon-document"></i>
+          <span>主页</span>
+        </el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>
+            <i class="el-icon-location"></i>
+            <span>视觉算法</span>
+          </template>
+          <el-menu-item index="1-1">人脸识别</el-menu-item>
+          <el-menu-item index="1-2">目标识别</el-menu-item>
+        </el-sub-menu>
+        <el-menu-item index="3">
+          <i class="el-icon-document"></i>
+          <span>关于</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+    <el-col :span="19">
+      <hello-world></hello-world>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
