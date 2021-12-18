409dda839c70c1c4b7b7fbe60ec388c09d135118
li1553770945 <1553770945@qq.com>
2021-12-19 01:29:47 +0800

    添加主页面内容

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,13 +2,18 @@
   <div class="main">
     <div class="collapse">
       <el-collapse accordion>
-        <el-collapse-item title="图片手绘风格转换" name="sift_matching">
+        <el-collapse-item
+          v-for="model in models"
+          :key="model.name"
+          :title="model.title"
+          :name="model.name"
+        >
           <h3>算法简介</h3>
-          <div>图片手绘风格转换使用了XXX方法，XXX....</div>
+          <div>{{ model.desc }}</div>
           <h3>参数说明</h3>
           <br />
           <div>
-            <el-table :data="sift_matching" stripe style="width: 100%">
+            <el-table :data="model.params" stripe style="width: 100%">
               <el-table-column prop="name" label="名称" />
               <el-table-column prop="type" label="类型" />
               <el-table-column prop="range" label="范围" />
@@ -27,13 +32,41 @@ export default {
   name: "Main",
   data() {
     return {
-      sift_matching: [
+      models: [
         {
-          name: "feature",
-          type: "string",
-          range: '"akaze", "kaze", "mser", "orb", "sift"',
-          default: '"sift"',
-          desc: "关键点特征",
+          name: "sift_matching",
+          title: "图片匹配",
+          desc: "图片匹配使用了sift特征...",
+          params: [
+            {
+              name: "feature",
+              type: "string",
+              range: '"akaze", "kaze", "mser", "orb", "sift"',
+              default: '"sift"',
+              desc: "关键点特征",
+            },
+            {
+              name: "feature",
+              type: "string",
+              range: '"akaze", "kaze", "mser", "orb", "sift"',
+              default: '"sift"',
+              desc: "关键点特征",
+            },
+          ],
+        },
+        {
+          name: "scanning",
+          title: "文档扫描",
+          desc: "文档扫描用于...",
+          params: [
+            {
+              name: "height",
+              type: "height",
+              range: '>0',
+              default: '500',
+              desc: "输出图片大小",
+            },
+          ],
         },
       ],
     };
