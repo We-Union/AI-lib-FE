e87589d4d349b77f5c7e286f4e51360cf004e0e9
li1553770945 <1553770945@qq.com>
2021-12-18 23:53:06 +0800

    实现删除参数功能

--- src/pages/MyPara.vue
+++ src/pages/MyPara.vue
@@ -1,19 +1,143 @@
 <template>
-  <div class="my-para">
+  <div class="add-para">
+    <br />
+    选择模型：
+    <el-select
+      v-model="select_model"
+      placeholder="选择模型"
+      @change="get_my_params()"
+    >
+      <el-option
+        v-for="item in model_list"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+
+    <br />
+    <br />
+    选择参数：
+    <el-select
+      v-model="select_params"
+      placeholder="选择参数"
+      @change="change_para()"
+    >
+      <el-option
+        v-for="item in params_list"
+        :key="item.value"
+        :label="item.name"
+        :value="item"
+      >
+      </el-option>
+    </el-select>
+    <br />
+    <br />
+    <div class="param-container">
+      <el-input
+        v-model="select_params.value"
+        :rows="10"
+        type="textarea"
+        placeholder="选择参数"
+      />
+    </div>
+    <br />
+    <div class="button">
+      <el-button @click="savePara()" type="primary">保存参数</el-button>
+      <el-button @click="deletePara()" type="danger">删除参数</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MyPara',
-    data () {
-        return {
-        
-        }
+  components: {},
+  data: () => ({
+    model_list: [
+      {
+        value: "sift_matching",
+        label: "图片匹配",
+      },
+      {
+        value: "transform_to_painting",
+        label: "图片手绘风格转换",
+      },
+      {
+        value: "reconstruct",
+        label: "超分辨率重建",
+      },
+      {
+        value: "scanning",
+        label: "文档扫描",
+      },
+    ],
+    select_model: "",
+    params_list: [],
+    select_params: "",
+  }),
+
+  methods: {
+    get_my_params() {
+      this.params_list = [];
+      this.select_params = "";
+      const axios = require("axios");
+      axios
+        .get("/parameter/getbymodel?model=" + this.select_model)
+        .then((response) => {
+          var data = response.data;
+          if (data.code == 0) {
+            this.params_list = data.data;
+          } else {
+            this.$message({
+              message: data.msg,
+              type: "error",
+            });
+          }
+        })
+        .catch((error) => {
+          this.$alert("" + error, "请求失败", {
+            confirmButtonText: "确定",
+          });
+        });
     },
-}
+    deletePara() {
+      const axios = require("axios");
+      axios
+        .delete("/parameter/delete", {data:{id: this.select_params.id }})
+        .then((response) => {
+          var data = response.data;
+          if (data.code == 0) {
+            this.$message({
+              message: "删除成功",
+              type: "success",
+            });
+            this.result = data;
+          } else {
+            this.$message({
+              message: data.msg,
+              type: "error",
+            });
+          }
+        })
+        .catch((error) => {
+          this.analyse_loading = false;
+          this.$alert("" + error, "请求失败", {
+            confirmButtonText: "确定",
+          });
+        });
+    },
+    change_para(val) {
+      console.log(val);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.json-editor {
+  width: 100%;
+  height: 30%;
+  text-align: center;
+}
 </style>
